2a0a0cc428507c749c81cd144e166e426e9f0310
Makhothi <147424358+Makhothi@users.noreply.github.com>
2024-04-12 18:33:40 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""Importing the needed framework"""
+""" Starts a Flash Web Application HBNB"""
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Function that returns a simple string"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Function that returns HBNB strint"""
-    return "HBNB"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
-if __name__ == '__main__':
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
